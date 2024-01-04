d5f80d5b8e569f544a4677725450940820506bbf
Hamza Amentag <hamentag1@gmail.com>
2024-01-03 23:57:17 -0600

    Added input validation/ only integers are accepted

--- math-tutor.cpp
+++ math-tutor.cpp
@@ -6,7 +6,10 @@
 #include <cstdlib>
 #include <ctime>
 #include <iomanip>
+#include <limits> 
 using namespace std;
+
+int getInteger();
 
 int main()
 {
@@ -22,7 +25,7 @@ int main()
         num1,      // To hold number 1
         num2;      // To hold number 2
 
-    char sign;     // To hold the sign
+    char sign;     // to hold the sign
 
     do
     {
@@ -35,13 +38,13 @@ int main()
              << "4. Division\n"
              << "5. Quit the program\n"
              << "Enter your choice: ";
-        cin >> choice;
+        choice = getInteger();
 
-        // Validate the input / choice
+        // Validate the menu select 
         while (choice < ADDITION || choice > QUIT)   
         {
             cout << "Please enter a valid choice: ";
-            cin >> choice;
+            choice = getInteger();
         } 
 
         // Process the student's choice
@@ -84,21 +87,21 @@ int main()
                 cout <<"----------\n";
 
                 // Get the answer
-                cin >> answer;
+                answer = getInteger();
 
                 // Check if the answer is correct
                if (answer == result)            
                 cout << "Congratulations. Your answer is correct.\n\n";
                else
                {
-                   cout << "\nWrong answer. The correct answer is: \n\n"
+                   cout << "\nIncorrect answer.\nSolution: \n\n"
                         << setw(8) << num1 << endl;
                    cout << sign << setw(7) << num2 << endl;
                    cout <<"----------\n"
                         << "=" << setw(7) << result << endl;
                }
             }
-            else  // Division
+            else  // Case: Division
             {
                 int remainder, quotient,        // To hold solutions
                     remainderAns, quotientAns;  // To hold the student's ansewrs
@@ -113,16 +116,16 @@ int main()
                 cout  << "\t/" << setw(7) << num2 << endl;
                 cout <<"\t----------\n"
                      << "Quotient : ";
-                cin >> quotientAns;
+                quotientAns = getInteger(); 
                 cout << "Remainder: ";
-                cin >> remainderAns;
+                remainderAns = getInteger();
 
                 // Check if the answers are corrects
                if (quotient == quotientAns && remainder == remainderAns)
                 cout << "Congratulations. Your answer is correct.\n\n"; 
                else
                {
-                   cout << "\nWrong answer. The correct answer is: \n\n"
+                   cout << "\nIncorrect answer.\nSolution: \n\n"
                         << "\t" << setw(8) << num1 << endl; 
                    cout  << "\t/" << setw(7) << num2 << endl;
                    cout <<"\t----------\n"
@@ -133,4 +136,21 @@ int main()
         }
     } while (choice != QUIT);
     return 0;      
+}
+
+int getInteger(){
+ int answer;
+
+    do {
+        std::cin >> answer;
+
+        // Check if the input is valid ( == integer)
+        if (std::cin.fail() || std::cin.peek() != '\n') {
+            std::cin.clear(); // Clear the error flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discard invalid input
+            std::cout << "Invalid input. Please enter an integer: ";
+        } else {
+            return answer;; // return the answer if the input is valid
+        }
+    } while (true);
 }
